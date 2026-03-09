a45d1f6a143b54cbda5d575d064beaecf4b8f565
KChethansai <kakunurichethansai@gmail.com>
2026-03-09 22:00:40 +0530

    Added Backend

--- JavaScript/Week Wise Assignments/Week-2/Day-4(26-02-2026)/Spread Operator/Exercise1.js
+++
@@ -1,7 +0,0 @@
-//Orignial Array
-let fruits = ["apple", "banana"];
-//Copied Array
- let moreFruits=[...fruits,'orange']
- /Check
- console.log('Original:',fruits)
- console.log('Copy:',moreFruits)
